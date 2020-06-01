ed311113290b3f9ea9c3cca85d34365e0648e9a1
lecuisto56 <lecuisto56@sfr.fr>
2020-06-01 19:31:42 +0200

    save1

--- shell1.sh
+++ shell1.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 NAMES=( John Eric Jessica )
 # write your code here
-NUMBERS=( 1 2 3 )
+NUMBERS=( 1 2 3 4 )
 STRINGS=( "hello" "world" )
 NumberOfNames=${#NAMES[@]}
 second_name=${NAMES[1]}
